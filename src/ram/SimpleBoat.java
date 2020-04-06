442627bdc743a8d4893d1f15285b640171ecf382
PrateekShrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2020-04-06 23:22:18 +0530

    Successfully create a great method that take two arguments as name and birthyear

---
+++ src/ram/SimpleBoat.java
@@ -0,0 +1,10 @@
+package ram;
+
+public class SimpleBoat {
+    static void great(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+    }
+
+}
