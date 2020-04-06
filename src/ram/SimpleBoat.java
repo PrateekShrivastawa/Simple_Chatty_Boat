6256fb2095d2d3dbfe794f18bb9a97be5f6e707a
PrateekShrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2020-04-06 23:25:41 +0530

    we create a remindname method to take input the name

--- src/ram/SimpleBoat.java
+++ src/ram/SimpleBoat.java
@@ -1,10 +1,18 @@
 package ram;
+import java.util.Scanner;
 
 public class SimpleBoat {
+    static Scanner sc = new Scanner(System.in);
+
     static void great(String assistantName, String birthYear) {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+    }
+
+    static void remindname() {
+        String name = sc.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
     }
 
 }
