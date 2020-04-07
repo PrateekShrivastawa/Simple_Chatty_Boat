34483e00c12d2087260027b00ee74bb621031107
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2020-04-07 11:37:42 +0530

    Update SimpleBoat.java

--- src/ram/SimpleBoat.java
+++ src/ram/SimpleBoat.java
@@ -1,20 +1,20 @@
 package ram;
 import java.util.Scanner;
-
+//Create a SimpleBoat interface which contain the method great,remindname,age,count,test and end.
 public interface SimpleBoat {
     static Scanner sc = new Scanner(System.in);
-
+    // This method has two parameters . which take name and year of invention.
     static void great(String assistantName, String inventedin) {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + inventedin + ".");
         System.out.println("Please, remind me your name.");
     }
-
+    // This method is use to print the name which we have already entered. 
     static void remindname() {
         String name = sc.nextLine();
         System.out.println("What a great name you have, " + name + "!");
     }
-
+    // This method is use to print the age of any person by input the remainder of the age by 3,5 and 7.
     static void age() {
         System.out.println("Let me guess your age.");
         System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
@@ -24,7 +24,7 @@ public interface SimpleBoat {
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
-
+    // This method is use to print the number using for loop.
     static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int num = sc.nextInt();
@@ -32,7 +32,7 @@ public interface SimpleBoat {
             System.out.println(i + "!");
         }
     }
-
+    // This method is use to predict the correct option.
     static void test() {
         System.out.println("Let's test your programming knowledge.");
         System.out.println("Why do we use methods?");
@@ -46,7 +46,7 @@ public interface SimpleBoat {
         }
         int b = sc.nextInt();
     }
-
+    // this method is use to print the last line.
     static void end() {
         System.out.println("Congratulations, have a nice day!"); // Do not change this text
     }
