50a1d20f6acab6f21ec654eb578726d5f122ef71
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2020-04-07 11:39:19 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import ram.SimpleBoat;
-
+// in Main method we simply call the method.
 public class Main implements SimpleBoat {
     public static void main(String[] args) {
         SimpleBoat.great("Prateek Shrivastawa", "2020");
